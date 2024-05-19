2dbda24c04c283a60ca819d0ac0ddf07c0c1906b
mis4mod7 <mis4mod7@gmail.com>
2024-05-19 13:13:06 +0430

    fix user migrations

--- database/migrations/2024_02_08_063611_create_users_table.php
+++ database/migrations/2024_02_08_063611_create_users_table.php
@@ -4,15 +4,13 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-
             $table->id();
             $table->string('name', 191)->nullable();
             $table->string('email', 191)->unique();
@@ -27,13 +25,9 @@ return new class extends Migration
             $table->integer('user_change_password_status')->default(0)->comment('0->not changed 1->changed');
             $table->string('lang', 5)->default('dr');
 
-            $table->foreign('department_id')
-            ->references('id')
-            ->on('departments');
-      $table->foreign('section_id')
-            ->references('id')
-            ->on('sections');
-            
+            $table->foreign('department_id')->references('id')->on('departments');
+            $table->foreign('section_id')->references('id')->on('sections');
+
             $table->timestamps();
             $table->softDeletes();
             $table->integer('created_by')->nullable();

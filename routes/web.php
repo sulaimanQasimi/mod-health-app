09eeb76d7928f254a35911ea4572f83f0719695c
Mohammad  Rafi10 <farzadqasimy@gmail.com>
2025-09-17 09:55:37 +0430

    Update hospitalizations route to use a root path for index method

--- routes/web.php
+++ routes/web.php
@@ -204,7 +204,7 @@ Route::group(['middleware' => ['auth']], function () {
 
     // Hospitalizations routes
     Route::prefix('hospitalizations')->name('hospitalizations.')->group(function () {
-        Route::get('index', [HospitalizationController::class, 'index'])->name('index');
+        Route::get('/', [HospitalizationController::class, 'index'])->name('index');
         Route::get('discharged', [HospitalizationController::class, 'discharged'])->name('discharged');
         Route::get('create', [HospitalizationController::class, 'create'])->name('create');
         Route::get('show/{hospitalization}', [HospitalizationController::class, 'show'])->name('show');

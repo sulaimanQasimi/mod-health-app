a34c95d67786aed71ab198131bacd07ec60d88ca
Mohammad  Rafi10 <152247100+Mohammadrafi10@users.noreply.github.com>
2025-10-02 10:17:02 +0430

    Update Persian datepicker integration and styles in master layout
    This commit enhances the Persian datepicker configuration in the master layout by adding comprehensive options for date selection, including month names, day titles, and display settings. Additionally, it updates the CSS and JS links to point to the new Persian datepicker files, ensuring proper styling and functionality. These changes improve the overall user experience with date selection across the application.

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -184,8 +184,39 @@
 <script>
     document.addEventListener('DOMContentLoaded', function () {
         $(".datepicker_dari").persianDatepicker({
-            formatDate: 'YYYY-MM-DD'
-        });
+            months: ["حمل", "ثور", "جوزا", "سرطان", "اسد", "سنبله", "میزان", "عقرب", "قوس", "جدی", "دلو", "حوت"],
+            dowTitle: ["شنبه", "یکشنبه", "دوشنبه", "سه شنبه", "چهارشنبه", "پنج شنبه", "جمعه"],
+        shortDowTitle: ["ش", "ی", "د", "س", "چ", "پ", "ج"],
+        showGregorianDate: !1,
+        persianNumbers: !0,
+        formatDate: "YYYY/MM/DD",
+        selectedBefore: !1,
+        selectedDate: null,
+        startDate: null,
+        endDate: null,
+        prevArrow: '\u25c4',
+        nextArrow: '\u25ba',
+        theme: 'default',
+        alwaysShow: !1,
+        selectableYears: null,
+        selectableMonths: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+        cellWidth: 25, // by px
+        cellHeight: 20, // by px
+        fontSize: 13, // by px
+        isRTL: !1,
+        calendarPosition: {
+            x: 0,
+            y: 0,
+        },
+        onShow: function () {},
+        onHide: function () {},
+        onSelect: function () {},
+        onRender: function () {}
+    });
+
+        
+
+
     });
 </script>
 

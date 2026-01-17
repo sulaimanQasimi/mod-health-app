b94790bfcd1e8ba375332b83b4d7382edab943e4
SulaimanQasimi <farzadqasimy@gmail.com>
2026-01-17 10:01:49 +0430

    Add Pharmacy Fulfillments Link in Sidebar: Introduced a new menu item in the sidebar for accessing pharmacy fulfillments, enhancing navigation and user experience within the application.

--- resources/views/layouts/partial/sidebar.blade.php
+++ resources/views/layouts/partial/sidebar.blade.php
@@ -234,6 +234,11 @@
                             <div>{{ localize('global.stock_overview') }}</div>
                         </a>
                     </li>
+                    <li class="menu-item {{ Route::is('pharmacy_fulfillments.*') ? 'active' : '' }}">
+                        <a href="{{ route('pharmacy_fulfillments.index') }}" class="menu-link">
+                            <div>{{ localize('global.pharmacy_fulfillments') }}</div>
+                        </a>
+                    </li>
                     <li class="menu-item {{ Route::is('incomes.*') ? 'active' : '' }}">
                         <a href="{{ route('incomes.index') }}" class="menu-link">
                             <div>{{ localize('global.stock_income') }}</div>

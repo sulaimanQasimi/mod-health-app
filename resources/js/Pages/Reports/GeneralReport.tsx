b047da3a7a6791e40c1d2afcf1589be3b3c1f6e9
Sulaiman Qasimi <farzadqasimy@gmail.com>
2026-06-22 10:21:02 +0430

    Update Inertia.js dependencies and enhance GeneralReport component: Upgrade @inertiajs/react and @inertiajs/core to version 3.4.0, and implement data fetching and display for the general report, including a table for department-wise patient counts.

--- resources/js/Pages/Reports/GeneralReport.tsx
+++ resources/js/Pages/Reports/GeneralReport.tsx
@@ -1,7 +1,35 @@
+import { useHttp } from '@inertiajs/react'
+import { useEffect, useState } from 'react';
+
 export default function GeneralReport() {
+
+    const {data,setData,get,processing}=useHttp({query:''});
+    const [appoinrmentReport,setAppoinrmentReport]=useState([]);
+    useEffect(()=>{
+        get('/react/api/reports/general/number-of-patients-base-on-department').then((response)=>{
+            setAppoinrmentReport((response as any).data);
+        });
+    },[]);
+
     return (
         <div>
             <h1>General Report</h1>
+            <table className="table table-bordered table-striped table-hover">
+                <thead>
+                    <tr>
+                        <th>Department Name</th>
+                        <th>Count</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {appoinrmentReport?.map((item:any)=>(
+                        <tr key={item.id}>
+                            <td>{item.department_name}</td>
+                            <td>{item.count}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
         </div>
     );
 }
